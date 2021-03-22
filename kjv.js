1133aad90e58fda7838590549ae52240a4c62c92
rdemo <randall@soulwinning.com>
2021-03-22 11:50:42 +0800

    updates

--- kjv.js
+++ kjv.js
@@ -1,6 +1,12 @@
 const kjv = require('./wholekjv.json')
 
 Object.keys(kjv).forEach((book) => {
-  console.log(book)
-  console.log(kjv[book].chapters.length)
+  // console.log(book)
+  // console.log(kjv[book].chapters.length)
 })
+
+getVerse('Galatians', 3, 16)
+
+function getVerse (book, chapter, verse) {
+  console.log(kjv[book].chapters[chapter - 1].verses[verse - 1][verse])
+}
