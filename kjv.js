26e542bf62eb732d43e398a902f90875129399f8
rdemo <randall@soulwinning.com>
2021-03-22 12:25:17 +0800

    update

--- kjv.js
+++ kjv.js
@@ -9,4 +9,5 @@ getVerse('Galatians', 3, 16)
 
 function getVerse (book, chapter, verse) {
   console.log(kjv[book].chapters[chapter - 1].verses[verse - 1][verse])
+              //kjv['Jude'].chapters[0].verses[1][verse]
 }
